0512a302adf6c232c065f50d40b574742a5f99f1
exp <arzuakov@yahoo.com>
2025-06-17 23:44:44 +0300

    17.06.2025

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto'
+import { UserService } from 'src/user/user.service'
 
 @Injectable()
 export class AuthService {
-	public async register(dto: RegisterDto){}
+	public constructor(private readonly userService: UserService){}
+
+	public async register(dto: RegisterDto) {
+		const user = await this.userService.findeByEmail(dto.email)
+		
+	}
 	public async login(){}
 	public async logout(){}
 	private async saveSession(){}
