8fb9265b1428e9c35fc0fec8389d6e194b272935
arzuakov@yahoo.com <arzuakov@yahoo.com>
2025-07-07 17:30:51 +0300

    07.07.2026

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,16 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common'
 import { RegisterDto } from './dto/register.dto'
 import { UserService } from 'src/user/user.service'
 import { AuthMetod, User } from 'prisma/__generated__'
+import { Request } from 'express'
+import { LoginDto } from './dto/login.dto'
+import { verify } from 'argon2'
 
 @Injectable()
 export class AuthService {
-	public constructor(private readonly userService: UserService){}
+	public constructor(private readonly userService: UserService) { }
 
-	public async register(dto: RegisterDto) {
+	public async register(req: Request, dto: RegisterDto) {
 		const isExist = await this.userService.findeByEmail(dto.email)
 
 		if (isExist) {
@@ -23,11 +26,38 @@ export class AuthService {
 			false
 		)
 
-		return this.saveSession(newUser);
+		return this.saveSession(req, newUser)
 	}
-	public async login(){}
-	public async logout(){}
-	private async saveSession(user: User) {
-		console.log('Session saved. User: ', user);
+	public async login(req: Request, dto: LoginDto) { 
+		const user = await this.userService.findeByEmail(dto.email)
+
+		if(!user || !user.password){
+			throw new NotFoundException("Пользователь не найден. Проверте введеные данные.")
+		}
+
+		const isValidPassword = await verify(user.password, dto.password)
+
+		if(!isValidPassword){
+			throw new UnauthorizedException("Неверный пароль")
+		}
+
+		return this.saveSession(req, user)
+	}
+
+	public async logout() { }
+
+	private async saveSession(req: Request, user: User) {
+		return new Promise((resolve, reject) => {
+			req.session.userId = user.id
+
+			req.session.save(err => {
+				if (err) {
+					return reject(
+						new InternalServerErrorException('Не удалось сохранить сессию. Проверте, правильно ли настроины параметры сессии'))
+				}
+				resolve({user})
+			})
+		})
+
 	}
 }
