83be2849d2785ed076b6d76ada7cefb0d2a8ea17
arzuakov@yahoo.com <arzuakov@yahoo.com>
2025-07-11 17:46:09 +0300

    11.07.2026

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,7 +1,4 @@
 import { Injectable, CanActivate, ExecutionContext, ForbiddenException, UnauthorizedException } from '@nestjs/common'
-import { Reflector } from '@nestjs/core'
-import { UserRole } from 'prisma/__generated__'
-import { ROLES_KEY } from '../decorators/roles.decorator'
 import { UserService } from 'src/user/user.service'
 import { Request } from 'express'
 
